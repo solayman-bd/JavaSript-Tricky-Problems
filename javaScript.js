0f9d6dabb14b051bc6797a72ce3d2458753b6bbc
solayman-bd <mdsolaymannuhin@gmail.com>
2021-12-02 08:50:37 +0600

    switch case problem added

--- javaScript.js
+++ javaScript.js
@@ -21,4 +21,4 @@ printMe(2); //no match found
 printMe(5); //no match found
 
 //Explanation:
-//printMe(2)-> (case n===1 )means (case false) cause because n===1 return false as 2 is passed. Same for other cases. For this reason we should use exact value in the case statement rather than logic.
+//printMe(2)-> (case n===1 )means (case false) cause because n===1 return false as 2 is passed. Same for other cases. For this reason we should use exact value in the case statement rather than logic. In another way, the value which is passed to the switch case wants to find the same in value in the case statement. So when n=2 is passed in the case statement case also wants to find case 2 but as there are some logical operators used for this reason in stead of finding exact value it is trying to match the boolean value
